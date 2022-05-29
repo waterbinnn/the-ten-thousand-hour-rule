e64c96217e9ca8d8e4ac463d5f2df792a7236359
Subin <ewaterbin@naver.com>
2022-05-29 19:14:54 +0900

    자바스크립트 추가 수정

---
+++ js/result.js
@@ -0,0 +1,65 @@
+const startButton = document.querySelector(".start-btn");
+const result = document.querySelector(".result");
+const modal = document.querySelector("#modal");
+const openButton = document.querySelector(".btn-go");
+const closeButton = document.querySelector(".btn-close");
+const shareButton = document.querySelector(".btn-share");
+const loading = document.querySelector(".result-loading");
+
+function calculator() {
+  const fieldValue = document.querySelector("#field-value");
+  let timeValue = document.querySelector("#time-value");
+  let timeValue_int = Number(timeValue.value);
+
+  const fieldResult = document.querySelector(".field-result");
+  const timeResult = document.querySelector(".time-result");
+
+  if (fieldValue.value == "") {
+    alert("입력되지 않았습니다.");
+    fieldValue.focus();
+    return false;
+  } else if (timeValue.value == "") {
+    alert("입력되지 않았습니다.");
+    timeValue.focus();
+    return false;
+  } else if (timeValue_int > 24) {
+    alert("잘못된 값입니다. 24이하의 값을 입력해 주세요.");
+    return false;
+  }
+  result.style.display = "none";
+  loading.style.display = "flex";
+
+  setTimeout(function () {
+    loading.style.display = "none";
+    result.style.display = "flex";
+    fieldResult.innerText = fieldValue.value;
+    timeResult.innerText = parseInt(10000 / timeValue_int, 10);
+  }, 1800);
+}
+
+function openModal() {
+  modal.style.display = "flex";
+}
+
+function closeModal() {
+  modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    closeModal();
+  }
+};
+
+function copyUrl() {
+  const url = window.location.href;
+
+  navigator.clipboard.writeText(url).then(() => {
+    alert("URL이 복사되었습니다");
+  });
+}
+
+shareButton.addEventListener("click", copyUrl);
+openButton.addEventListener("click", openModal);
+closeButton.addEventListener("click", closeModal);
+startButton.addEventListener("click", calculator);
